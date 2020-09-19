dd9fc8f3699c65e263cd335e3a058dffb9b0f174
Harsh Popat <harshpopat75@gmail.com>
2020-09-19 19:51:42 +0530

    addition of enemy and its back and forth movement

--- Game/pygame1.py
+++ Game/pygame1.py
@@ -32,10 +32,10 @@ class player(object):
                   
             if not (self.standing):
                   if self.left:
-                        win.blit(walkLeft[self.walkcount//3], (self.x, self.y))
+                        win.blit(walkLeft[self.walkcount//3], (self.x,self.y))
                         self.walkcount +=1
                   elif self.right:
-                        win.blit(walkRight[self.walkcount//3], (self.x, self.y))
+                        win.blit(walkRight[self.walkcount//3], (self.x,self.y))
                         self.walkcount +=1
             else:
                   if self.right:
@@ -55,10 +55,51 @@ class projectile(object):
       def draw(self,win):
                   pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
                   
+class enemy(object):
+      walkRight = [pygame.image.load('R1E.png'), pygame.image.load('R2E.png'), pygame.image.load('R3E.png'), pygame.image.load('R4E.png'), pygame.image.load('R5E.png'), pygame.image.load('R6E.png'), pygame.image.load('R7E.png'), pygame.image.load('R8E.png'), pygame.image.load('R9E.png'), pygame.image.load('R10E.png'), pygame.image.load('R11E.png')]
+      walkLeft = [pygame.image.load('L1E.png'), pygame.image.load('L2E.png'), pygame.image.load('L3E.png'), pygame.image.load('L4E.png'), pygame.image.load('L5E.png'), pygame.image.load('L6E.png'), pygame.image.load('L7E.png'), pygame.image.load('L8E.png'), pygame.image.load('L9E.png'), pygame.image.load('L10E.png'), pygame.image.load('L11E.png')]
+
+      def __init__(self, x, y, width, height, end):
+            self.x = x
+            self.y = y
+            self.width = width
+            self.height = height
+            self.end = end
+            self.path = [self.x, self.end]
+            self.walkcount = 0
+            self.vel = 3
+
+      def draw(self,win):
+            self.move()
+            if self.walkcount + 1 >= 33:
+                  self.walkcount = 0
+
+            if self.vel > 0:
+                  win.blit(self.walkRight[self.walkcount //3], (self.x, self.y))
+                  self.walkcount += 1
+            else:
+                  win.blit(self.walkLeft[self.walkcount //3], (self.x, self.y))
+                  self.walkcount += 1
+                        
+
+      def move(self):
+            if self.vel > 0:
+                  if self.x + self.vel <self.path[1]:
+                        self.x += self.vel
+                  else:
+                        self.vel = self.vel * -1
+                        self.walkcount = 0
+            else:
+                  if self.x - self.vel > self.path[0]:
+                        self.x += self.vel
+                  else:
+                        self.vel = self.vel * -1
+                        self.walkcount = 0
 
 def redrawGameWindow():
       win.blit(bg, (0,0))
       man.draw(win)
+      goblin.draw(win)
       for bullet in bullets:
             bullet.draw(win)
 
@@ -66,6 +107,7 @@ def redrawGameWindow():
 
 #mainloop
 man = player(300, 410, 64, 64)
+goblin = enemy(100, 410, 64, 64, 450)
 bullets = []
 run = True
 while run:

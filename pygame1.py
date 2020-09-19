537bb7f9a3adaad2e837d03646c43385fcc6803a
Harsh Popat <harshpopat75@gmail.com>
2020-09-19 11:58:18 +0530

    jumping movements added

--- pygame1.py
+++ pygame1.py
@@ -11,9 +11,12 @@ width = 40
 height = 60
 vel = 5
 
+isjump = False
+jumpcount = 10
+
 run = True
 while run:
-      pygame.time.delay(100)
+      pygame.time.delay(50)
 
       for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -25,11 +28,26 @@ while run:
             x -= vel
       if keys[pygame.K_RIGHT] and x < 500 - width - vel:
             x += vel
-      if keys[pygame.K_UP] and y > vel:
-            y -= vel
-      if keys[pygame.K_DOWN] and y < 500 - height - vel:
-            y += vel
+      if not(isjump):
+          if keys[pygame.K_UP] and y > vel:
+                y -= vel
+          if keys[pygame.K_DOWN] and y < 500 - height - vel:
+                y += vel
+          if keys[pygame.K_SPACE]:
+              isjump = True
+        
+      else:
+          if jumpcount >= -10:
+              neg = 1
+              if jumpcount < 0:
+                  neg = -1
+              y -= (jumpcount ** 2) * 0.5 * neg
+              jumpcount -= 1
+          else:
+                  isjump = False
+                  jumpcount = 10
 
+          
       win.fill((0,0,0))
       pygame.draw.rect( win, (255, 0, 0), (x, y, width, height) )
       pygame.display.update()
